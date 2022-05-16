c4c1cfd857c4dd36c63ea99d6967c48f94fa13b3
imbatbyar <imbatbayar@gmail.com>
2022-05-16 22:35:33 +0800

    sanhuugiin modul oruulaw

--- app.js
+++ app.js
@@ -1,25 +1,80 @@
-// Дэлгэцтэй ажиллах контроллор
-var uiController = (function () {})();
-// Санхүүтэй ажиллах контроллор
-var financeController = (function () {})();
-// Программын холбогч контроллор
+// ------------Дэлгэцтэй ажиллах контроллор--------------
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
+// --------Санхүүтэй ажиллах контроллор---------------
+var financeController = (function () {
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      inc: [],
+      exp: [],
+    },
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
+})();
+// --------Программын холбогч контроллор--------------
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
-    // 1. Оруулах өгөгдөлийг дэлгэцнээс олж авна
+    // 1. Оруулах өгөгдөлийг дэлгэцнээс олж авна.
+    console.log(uiController.getInput());
     // 2. Олж авсан өгөгдүүлдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
     // 3. Олж авсан өгөгдүүдээ вэб дээр тохирох хэсэгт нь гаргана.
     // 4. Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл тооцоог дэлгэц дээр гаргана.
-    console.log("Дэлгэцээс өгөгдөл авах хэсэг");
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    ctrlAddItem();
-  });
-
-  document.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13 || event.which === 13) {
+  var setupEventListners = function () {
+    var DOM = uiController.getDOMstrings();
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
-    }
-  });
+    });
+
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  return {
+    init: function () {
+      console.log("Application started...");
+      setupEventListners();
+    },
+  };
 })(uiController, financeController);
+
+appController.init();
